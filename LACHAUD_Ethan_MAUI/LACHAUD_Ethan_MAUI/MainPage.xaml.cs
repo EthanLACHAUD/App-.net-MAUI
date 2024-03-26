df6a09f6d1e7354e9b5e73466a1016dc398f58c3
EthanLACHAUD <ethan.lachaud@etu.unilim.fr>
2024-03-26 17:16:49 +0100

    debut étape deux

--- LACHAUD_Ethan_MAUI/LACHAUD_Ethan_MAUI/MainPage.xaml.cs
+++ LACHAUD_Ethan_MAUI/LACHAUD_Ethan_MAUI/MainPage.xaml.cs
@@ -7,6 +7,18 @@
         {
             InitializeComponent();
         }
+        //affiche remplace les deux images par un gif avec un bouton
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var image1 = (Image)FindByName("image1");
+            var image2 = (Image)FindByName("image2");
+            var gifImage = (Image)FindByName("gif");
+            //affiche le gif
+            gifImage.IsVisible = true;
+            //cache les deux images
+            image1.IsVisible = false;
+            image2.IsVisible = false;
+        }
         
     }
 
