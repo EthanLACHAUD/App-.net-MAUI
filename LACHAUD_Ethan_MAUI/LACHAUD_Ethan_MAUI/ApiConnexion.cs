2625bb96a745779c78be9632cf0143f220527a98
EthanLACHAUD <ethan.lachaud@etu.unilim.fr>
2024-04-13 15:34:59 +0200

    etape 3 sans verif, verif faite avec etape 4

---
+++ LACHAUD_Ethan_MAUI/LACHAUD_Ethan_MAUI/ApiConnexion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace LACHAUD_Ethan_MAUI;
+
+public class ApiConnexion
+{
+    public ObservableCollection<Futurama> Characters { get; } = new ObservableCollection<Futurama>();
+
+    public ApiConnexion()
+    {
+        Task.Run(() => GetCharacters());
+
+    }
+    
+    private async Task GetCharacters()
+    {
+        var client = new HttpClient();
+        var response = await client.GetStringAsync("https://api.sampleapis.com/futurama/characters");
+        var characters = JsonSerializer.Deserialize<List<Futurama>>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        
+        foreach (var character in characters)
+        {
+            Characters.Add(character);
+        }
+    }
+}
+
---
+++ LACHAUD_Ethan_MAUI/LACHAUD_Ethan_MAUI/Futurama.cs
@@ -0,0 +1,11 @@
+﻿namespace LACHAUD_Ethan_MAUI;
+
+public class Futurama
+{
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string Planet { get; set; }
+    public string Profession { get; set; }
+    public string Image { get; set; }
+    
+}
